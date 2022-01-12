be6174df65b0b4417e4722ecd1dd22bd7832dd15
Im Chaewon <lisa425@sookmyung.ac.kr>
2022-01-12 13:27:19 +0900

    1-11.대문자 찾기

--- 1M-3rd/0112-3.js
+++ 1M-3rd/0112-3.js
@@ -8,6 +8,7 @@
 해당 문자의 개수를 출력한다.
 */
 
+//풀이:문자열 순회하면서 주어진 단어와 같으면 answer의 값을 +1 시킴
 function solution(string,word){
     let answer = 0; //answer초기화
     for(str of string){
@@ -20,18 +21,14 @@ function solution(string,word){
 
 console.log(solution('COMPUTERPROGRAMMING','R'))
 
-/* 풀이:
-문자열 순회하면서 주어진 단어와 같으면 answer의 값을 +1 시킴
 
-[더 좋은 풀이!!!!]
-문자열을 split()메소드를 이용해서 주어진 단어(R)을 기준으로 자르면 문자열이 조각난다.
-예를들면 'COMPUTERPROGRAMMING'에서 'R'을 기준으로 자르면 ['COMPUTE,P,OG,AMMING']으로 총 3번 잘려서 4조각이 됨.
-그러니까 split으로 문자열을 자르고 리턴값에서 -1을 하면 문자열이 몇 개인지 더 쉽게 알 수 있다.
-
-더 좋은 답안:
-function solution(string,word){
+//더 좋은 풀이!!!
+function solution2(string,word){
     let answer = string.split(word).length - 1;
     return answer
 }
-
+/*
+문자열을 split()메소드를 이용해서 주어진 단어(R)을 기준으로 자르면 문자열이 조각난다.
+예를들면 'COMPUTERPROGRAMMING'에서 'R'을 기준으로 자르면 ['COMPUTE,P,OG,AMMING']으로 총 3번 잘려서 4조각이 됨.
+그러니까 split으로 문자열을 자르고 리턴값에서 -1을 하면 문자열이 몇 개인지 더 쉽게 알 수 있다.
  */
