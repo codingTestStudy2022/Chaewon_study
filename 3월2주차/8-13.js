2a8968386832740b71fc8d8a963c5150d8dbfdc2
Im Chaewon <lisa425@sookmyung.ac.kr>
2022-03-14 19:15:23 +0900

    3-3주차

--- 3월2주차/8-13.js
+++ 3월2주차/8-13.js
@@ -1,12 +1,22 @@
 //8-13.수열 추측하기
 function solution(n,r){
-    //일반 조합수 구하기
     let answer;
-    function DFS(n,r){
+    let dy = Array.from(Array(11),()=>Array(11).fill(0))
+    let ch = Array.from({length:n+1},()=>0);
+    let p = Array.from({length:n},()=>0);
+    let b = Array.from({length:n},()=>0);
+    
+
+    function combi(n,r){
+        if(dy[n][r]>0) return dy[n][r]
         if(n===r || r===0) return 1;
-        else return DFS(n-1,r-1)+DFS(n-1,r);
+        else return dy[n][r]=combi(n-1,r-1)+combi(n-1,r);
     }
-    answer = DFS(n,r)
+    function DFS(L,sum){
+
+    }
+    
+    answer = DFS(0,0)
     return answer
 }
 
---
+++ 3월3주차/lv1-1.js
@@ -0,0 +1,6 @@
+//프로그래머스 Lv1.신고 결과 받기
+
+function solution(id_list, report, k) {
+    var answer = [];
+    return answer;
+}
